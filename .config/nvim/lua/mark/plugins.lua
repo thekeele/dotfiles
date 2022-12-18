99391a44aad6fb3924021ec05c2ded8b56e1986f
Mark Keele <mark@keele.codes>
2022-12-18 11:49:13 -0500

    add plugin to easily comment lines

--- .config/nvim/lua/mark/plugins.lua
+++ .config/nvim/lua/mark/plugins.lua
@@ -89,6 +89,9 @@ return packer.startup(function(use)
         'lewis6991/gitsigns.nvim'
         -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
     }
+								
+    -- plugin to easily comment lines and blocks
+    use {'numToStr/Comment.nvim'}
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
