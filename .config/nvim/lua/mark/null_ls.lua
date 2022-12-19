70e4cbb8733a9ba632a17c4c10d9eb9de7faae8f
Mark Keele <mark@keele.codes>
2022-12-19 17:43:56 -0500

    enable credo for null-ls

--- .config/nvim/lua/mark/null_ls.lua
+++ .config/nvim/lua/mark/null_ls.lua
@@ -5,7 +5,7 @@ null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.mix,
 		null_ls.builtins.formatting.surface,
-		-- null_ls.builtins.diagnostics.credo,
+		null_ls.builtins.diagnostics.credo,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.completion.luasnip,
 	},
